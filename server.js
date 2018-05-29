ea689e30ace966f97dd4ed89832e7940361a8335
Joe Wright <joewrightart@gmail.com>
2018-05-29 14:51:21 -0400

    added passport, building logic

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var session = require("express-session");
+var passport = require("./config/passport");
 var app = express();
 var exphbs = require("express-handlebars");
 
 var PORT = process.env.PORT || 8080;
+var db = require("./models");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -15,6 +21,8 @@ var routes = require("./controllers/recipes_controller.js");
 
 app.use(routes);
 
-app.listen(PORT, function() {
-  console.log("Server listening on: http://localhost:" + PORT);
-});
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });  
+})
