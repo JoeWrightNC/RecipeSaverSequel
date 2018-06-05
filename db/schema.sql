4d83de626024baff77306b5e06e730b217412d69
Joe Wright <joewrightart@gmail.com>
2018-06-05 15:05:13 -0400

    optimizing to deploy to JawsDB and Heroku

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,3 @@
 DROP DATABASE IF EXISTS recipes_db;
 CREATE DATABASE recipes_db;
-USE recipes_db;
 
-CREATE TABLE recipes
-(
-	id int NOT NULL AUTO_INCREMENT,
-	recipeName VARCHAR(255) NOT NULL,
-  recipeUrl VARCHAR(8000) NOT NULL,
-	tried BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
-);
